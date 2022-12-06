e9d4c115b8795b4a75740f22f71973f33e25e014
Matteo Vidali <mvidali@iu.edu>
2022-12-06 19:47:56 +0000

    Added functioning i2c driver and updated requirements

--- Data_Drivers/gsr.py
+++ Data_Drivers/gsr.py
@@ -3,14 +3,26 @@ import smbus
 class GSR:
     def __init__(self):
         self.channel =1
-        self.address=0x5
+        self.address=0x8
         self.bus = smbus.SMBus(self.channel)
+        
+        if not self._connect():
+            print("NO DEVICE FOUND/CONNECTION ERROR")
+            exit(1)
 
+    def _connect(self):
+        try:
+            return self.bus.read_byte_data(self.address,1)
+        except:
+            return 0
+
+    
     def read(self):
-        return self.bus.read_byte_data(self.address)
+        return self.bus.read_byte_data(self.address, 1)
 
 if __name__ == "__main__":
     gsr = GSR()
-
-    print(gsr.read())
+    
+    for i in range(10):
+        print(gsr.read())
         
