cb7b2772e0ef581f18f84dc3db3b43c4157ad6c9
Matteo Vidali <mvidali@iu.edu>
2023-01-24 20:57:48 +0000

    Fixed error in i2c connection

--- Arduino/GSR/GSR.ino
+++ Arduino/GSR/GSR.ino
@@ -1,13 +1,21 @@
 #include <Wire.h>
 
+int led = 13;
+
 void setup() {
-    Wire.begin(8);
+    Wire.begin(0x08);
     Wire.onRequest(req_handler);
+    pinMode(led, OUTPUT);
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
 }
 
 void loop() {
 }
 
 void req_handler(){
+    digitalWrite(led, HIGH);
     Wire.write(analogRead(A0));
+    digitalWrite(led, LOW);
 }
