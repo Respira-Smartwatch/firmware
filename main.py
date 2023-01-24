e309757535d1cf41b1a956de996b6f54f29f5970
Matteo Vidali <mvidali@iu.edu>
2023-01-24 11:02:00 -0500

    Added Main function: Data flow unnecessary, this flow is more concise

--- main.py
+++ main.py
@@ -1,13 +1,59 @@
-from Data_Drivers import DataFlow, GSR
-from ReSpeaker_Drivers import Mics, Button
+import serial
+from Models import SpeechEmotionClassifier, GSRClassifier
 
-def main():
-    mic = Mics()
-    gsr = GSR()
+_ser = None
 
-    df = DataFlow([gsr.read], True, True, 'test.out')
-    button = Button(on_button_func=df.pause)
+def push_to_tty(*args) -> bool:
+    global _ser
+    if not _ser:
+        try:
+            _ser = serial.Serial("/dev/ttyS0",
+                                 baudrate=9600,
+                                 parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE,
+                                 bytesize=serial.EIGHTBITS,
+                                 bytesize=serial.EIGHTBITS,
+                                 timeout=1)
+        except not _ser.is_open:
+            print("ERROR: SERIAL PORT UNABLE TO OPEN!")
+            return False
 
+    for data in args:
+        try:
+            _ser.write(data.to_bytes(1, 'little'))
+        except serial.SerialTimeoutException: 
+            print("Serial Timeout while Writing...")
+            return False
+    
+    return True
+    
+
+def main(plotting=False):
+    audio_classifier = SpeechEmotionClassifier()
+    gsr_classifier = GSRClassifier()
+
+    # completely arbitrary value that will need to be 
+    # Set experimentally:
+    gsr_gating_threshold = 25
+
+    while True:
+        anxiety_level = gsr_classifier.predict()
+
+        emotion = None
+        if anxiety_level >= gsr_gating_threshold:
+            emotion = audio_classifier.predict()
+        
+        print(f"Anxiety Level as given by GSR: {anxiety_level} / {gsr_gating_threshold}")
+        
+        if emotion:
+            print(f"Emotion as given by SE Classifier: {emotion}")
+
+        # Handles plotting via tty
+        if plotting:
+            complete = push_to_tty(anxiety_level, emotion)
+            if not complete:
+                print("Error while writing to serial port")
+    
 
 if __name__ == "__main__":
     main()
