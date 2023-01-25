a86951765fc588c1e82226d2d963edfe8b379ac1
Matteo Vidali <mvidali@iu.edu>
2023-01-25 07:04:42 -0500

    Handle IO error in GSR Driver

--- Drivers/GSRDriver.py
+++ Drivers/GSRDriver.py
@@ -6,6 +6,7 @@ class GSRDriver:
         self.address = 0x8
         self.bus = smbus.SMBus(self.channel)
         self.sample_rate = f_s
+        self.error_cnt = 10
 
         if not self._connect():
             print("NO Device Found / Connection Error")
@@ -18,8 +19,13 @@ class GSRDriver:
         except:
             return 0
     
+    #TODO: Currently does not regulate rate : FIX THIS
     def read_once(self):
-        return self.bus.read_byte_data(self.address, 1)
+        try:
+            return self.bus.read_byte_data(self.address, 1)
+        except OSError:
+            self.error_cnt -= 1
+            return self.read_once() if self.error_cnt else -1
     
     def get_sample(self):
         return [self.read_once() for _ in range(self.sample_rate)]
