07295a6e671e9e13b18a4fdaaea244724e4fa475
Joseph Bellahcen <joeclb@icloud.com>
2023-02-05 19:36:53 -0500

    Enabled LED interface

--- Drivers/__init__.py
+++ Drivers/__init__.py
@@ -1,3 +1,4 @@
 from .AudioDriver import AudioDriver
 from .GSRDriver import GSRDriver
+from .apa102 import APA102
 
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from Models import GSRClassifier, SpeechEmotionClassifier
+from Drivers import APA102 
 
 if __name__ == "__main__":
     # Models
     gsr_model = GSRClassifier()
     speech_model = SpeechEmotionClassifier()
+
+    # LED (should be wrapped in new class)
+    activity_led = 0
+    gsr_led = 1
+    speech_led = 2
+    led = APA102(3)
+    led.clear_strip()
+
+    led.set_pixel(activity_led, 0, 255, 0, 5)
+    led.show()
 
     # Model parameters
     speech_duration_s = 5.0
