5716b80ee436b78c646242e784b932bc0bdfed5b
Joseph Bellahcen <joeclb@icloud.com>
2023-03-06 14:04:47 -0500

    Increased LED intensity

--- Drivers/LEDArray.py
+++ Drivers/LEDArray.py
@@ -1,5 +1,6 @@
 from .apa102 import APA102
 
+
 class LEDArray:
     def __init__(self):
         self.idx = 0
@@ -7,17 +8,17 @@ class LEDArray:
 
     def idle(self):
         """In the idle state, the LED is green"""
-        self.driver.set_pixel(self.idx, 0, 255, 0, 5)
+        self.driver.set_pixel(self.idx, 0, 255, 0, 10)
         self.driver.show()
 
     def speech(self):
         """While recording speech, the LED is blue"""
-        self.driver.set_pixel(self.idx, 0, 0, 255, 5)
+        self.driver.set_pixel(self.idx, 0, 0, 255, 10)
         self.driver.show()
 
     def gsr(self):
         """While sampling GSR, the LED is purple"""
-        self.driver.set_pixel(self.idx, 255, 0, 255, 5)
+        self.driver.set_pixel(self.idx, 255, 0, 255, 10)
         self.driver.show()
 
     def result(self):
